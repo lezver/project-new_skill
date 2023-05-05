b3ca787f05e39b961b26f8e7fdb0fa7b1a4f9d5c
Vladislav <102545184+lezver@users.noreply.github.com>
2023-05-05 16:33:43 +0300

    ready to work

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/header/header';
+import './js/categories/categories';
+import './js/home/home';
+import './js/loader/loader';
+import './js/modal-book/modal-book';
+import './js/scroll-up/scroll-up';
+import './js/shopping-list/shopping-list';
+import './js/sing-up/sing-up';
+import './js/support-ukraine/support-ukraine';

56873cc91bd0cfc2a95504c6cfd3bbf6549f37eb
Oleh Linskyi <118620479+OlehLinskyy@users.noreply.github.com>
2023-05-06 22:48:44 +0300

    add markup and logic to shopping list

---
+++ src/js/shopping-list/shopping-list.js
@@ -0,0 +1,115 @@
+
+import amazonImage1 from '../../images/png/modal-book/amazon.png';
+import amazonImage2 from '../../images/png/modal-book/amazon@2x.png';
+
+import appleImage1 from '../../images/png/modal-book/book.png'
+import appleImage2 from '../../images/png/modal-book/book@2x.png'
+
+import bookshopImage1 from '../../images/png/modal-book/book-shop.png'
+import bookshopImage2 from '../../images/png/modal-book/book-shop@2x.png'
+
+const galleryEl = document.querySelector('.gallery');
+const emptyListEl = document.querySelector('.shopping-list-empty');
+const savedSettings = localStorage.getItem('shopping_list');
+let parsedSettings = []
+if(savedSettings) { 
+  parsedSettings = JSON.parse(savedSettings);
+}
+innerMarkup()
+
+function qqq(event) {
+  const target = event.target.closest('.button');
+  console.log(target.id)
+  const index = parsedSettings.map(element => element._id).indexOf(target.id)
+  console.log(index)
+  parsedSettings.splice(index,1)
+  // localStorage.setItem('shopping_list', JSON.stringify(parsedSettings))
+  console.log(parsedSettings)
+  innerMarkup()
+}
+
+function innerMarkup(){
+  removeEventListener('click', qqq)
+  if (parsedSettings.length) {
+    const markup = parsedSettings.map(dataItem => buildBooks(dataItem)).join('');
+    galleryEl.innerHTML = markup
+    onListener()
+  }
+  else {
+    emptyListEl.innerHTML = buildEmptyList()
+    galleryEl.innerHTML = ``
+  }
+};
+
+function onListener() {
+  const buttonEl = document.querySelectorAll('.button');
+  buttonEl.forEach(element => {
+    element.addEventListener('click', qqq)
+  })
+};
+
+function buildEmptyList(){
+  return `<div class="empty-list">
+    <p>This page is empty, add some books and proceed to order.</p>
+  </div>`
+}
+buildBooks(parsedSettings);
+function buildBooks({book_image,title,list_name,description,author,_id,buy_links}) {
+  let urlAmazon = ''
+  let urlApplebooks = ''
+  let urlBookshop = ''
+  if(buy_links) {
+    const names = buy_links.map(item => item.name)
+    const indexAmazon = names.indexOf('Amazon')
+    urlAmazon = buy_links[indexAmazon].url;
+
+    const indexApplebooks = names.indexOf('Apple Books')
+    urlApplebooks = buy_links[indexApplebooks].url;
+
+    const indexBookshop = names.indexOf('Bookshop')
+    urlBookshop = buy_links[indexBookshop].url;
+  }
+  return `
+  <li class="border shopping-list-item">
+      <img src="${book_image}" alt="${title}" class="book_image">
+    <div class = "book-info">
+        <div>
+            <div class="qwe">
+                <div>
+                    <h3 class = "book-tittle">${title}</h3>
+                    <p class = "list-name">${list_name}</p>
+                </div>
+                <button id="${_id}" class="button">
+                    <svg class="icon">
+                        <use href="/icons.adfc4680.svg#icon-basket"></use>
+                    </svg>
+                </button>
+            </div>
+        <div  class="description-info">
+            <p class = "description">${description}</p>
+        </div>
+    </div>
+    <div class="asd">
+        <p class = "author">${author}</p>
+        <ul class="list">
+          <li>
+            <a target="_blank" href="${urlAmazon}">
+              <img src="${amazonImage1}" alt="amazon" class="link-amazon">
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="${urlApplebooks}">
+              <img src="${appleImage1}" alt="applebooks" class="link-applebooks">
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="${urlBookshop}">
+              <img src="${bookshopImage1}" alt="bookshop" class="link-bookshop">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </li>
+  `;
+}

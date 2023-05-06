314bfb392530389bf59ff7638a7a88968af6410e
Vladislav <102545184+lezver@users.noreply.github.com>
2023-05-06 14:47:16 +0300

    test

---
+++ src/js/home/home.js
@@ -0,0 +1,204 @@
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import { homeItems, mobCap, homeTitle, categoryBtn } from './refsOfTags';
+
+const URL = 'https://books-backend.p.goit.global/books/top-books';
+
+const checkBtn = async e => {
+  const categoryBtn = document.querySelector('.category-btn');
+  if (e.target.className === categoryBtn.className) {
+    const category = e.target.dataset.category.replace(/ /g, '+');
+
+    const fetchCategoryBooks = await axios.get(
+      'https://books-backend.p.goit.global/books/category?category=' + category
+    );
+
+    e.target.disabled = true;
+
+    setTimeout(() => {
+      e.target.disabled = false;
+    }, 1000);
+  }
+};
+
+const createMurkupBestForMob = arr => {
+  const maukup = arr.reduce((acc, { books: [book] }) => {
+    const { book_image, title, author, list_name } = book;
+    const img = `<img src="${book_image}" alt="book" />`;
+    return (
+      acc +
+      `
+      <li class="item__mob">
+        <p>${list_name}</p>
+        <div>
+          ${img ? img : refs.mobCap}
+          <button type="button" data-open-modal>quick view</button>
+        </div>
+        <h2>${title}</h2>
+        <span>${author}</span>
+        <button class="category-btn" type="button" data-category="${list_name}">see more</button>
+      </li>
+      `
+    );
+  }, '');
+
+  refs.homeTitle.innerHTML = '<span>Best Sellers</span> Books';
+
+  refs.homeItems.innerHTML = maukup;
+
+  refs.homeItems.addEventListener('click', checkBtn);
+};
+const createMurkupBestForTab = arr => {
+  const maukup = arr.reduce(
+    (acc, { list_name, books: [firstbook, secondBook, thirdBook] }) => {
+      const img1 = `<img src="${firstbook.book_image}" alt="book" />`;
+      const img2 = `<img src="${secondBook.book_image}" alt="book" />`;
+      const img3 = `<img src="${thirdBook.book_image}" alt="book" />`;
+
+      return (
+        acc +
+        `
+      <li class="item__tab">
+      <p>${list_name}</p>
+        <ul>
+          <li>
+            <div>
+              ${img1 ? img1 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${firstbook.title}</h2>
+            <span>${firstbook.author}</span>
+          </li>
+          <li>
+            <div>
+              ${img2 ? img2 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${secondBook.title}</h2>
+            <span>${secondBook.author}</span>
+          </li>
+          <li>
+            <div>
+              ${img3 ? img3 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${thirdBook.title}</h2>
+            <span>${thirdBook.author}</span>
+          </li>
+        </ul>
+        <button class="category-btn" type="button" data-category="${list_name}">
+          see more
+        </button>
+      </li>
+      `
+      );
+    },
+    ''
+  );
+
+  refs.homeTitle.innerHTML = '<span>Best Sellers</span> Books';
+
+  refs.homeItems.innerHTML = maukup;
+
+  refs.homeItems.addEventListener('click', checkBtn);
+};
+const createMurkupBestForDesk = arr => {
+  const maukup = arr.reduce(
+    (
+      acc,
+      {
+        list_name,
+        books: [firstbook, secondBook, thirdBook, fourthBook, fifthBook],
+      }
+    ) => {
+      const img1 = `<img src="${firstbook.book_image}" alt="book" />`;
+      const img2 = `<img src="${secondBook.book_image}" alt="book" />`;
+      const img3 = `<img src="${thirdBook.book_image}" alt="book" />`;
+      const img4 = `<img src="${fourthBook.book_image}" alt="book" />`;
+      const img5 = `<img src="${fifthBook.book_image}" alt="book" />`;
+
+      return (
+        acc +
+        `
+      <li class="item__desk">
+      <p>${list_name}</p>
+        <ul>
+          <li>
+            <div>
+              ${img1 ? img1 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${firstbook.title}</h2>
+            <span>${firstbook.author}</span>
+          </li>
+          <li>
+            <div>
+              ${img2 ? img2 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${secondBook.title}</h2>
+            <span>${secondBook.author}</span>
+          </li>
+          <li>
+            <div>
+              ${img3 ? img3 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${thirdBook.title}</h2>
+            <span>${thirdBook.author}</span>
+          </li>
+          <li>
+            <div>
+              ${img4 ? img4 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${fourthBook.title}</h2>
+            <span>${fourthBook.author}</span>
+          </li>
+          <li>
+            <div>
+              ${img5 ? img5 : refs.mobCap}
+              <button type="button" data-open-modal>quick view</button>
+            </div>
+            <h2>${fifthBook.title}</h2>
+            <span>${fifthBook.author}</span>
+          </li>
+        </ul>
+        <button class="category-btn" type="button" data-category="${list_name}">
+          see more
+        </button>
+      </li>
+      `
+      );
+    },
+    ''
+  );
+
+  refs.homeTitle.innerHTML = '<span>Best Sellers</span> Books';
+
+  refs.homeItems.innerHTML = maukup;
+
+  refs.homeItems.addEventListener('click', checkBtn);
+};
+
+const checkOfWidth = data => {
+  if (window.innerWidth <= 577) {
+    createMurkupBestForMob(data);
+  } else if (window.innerWidth > 577 && window.innerWidth < 851) {
+    createMurkupBestForTab(data);
+  } else {
+    createMurkupBestForDesk(data);
+  }
+};
+
+const fetchTopBooks = async url => {
+  try {
+    const response = await axios.get(url);
+
+    await checkOfWidth(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchTopBooks(URL);
---
+++ src/js/home/refsOfTags.js
@@ -0,0 +1,12 @@
+export default refs = {
+  homeItems: document.querySelector('.home__items'),
+  homeTitle: document.querySelector('.home__title'),
+  mobCap: `<img
+  srcset="
+    ./images/png/home/mob/icon.png    1x,
+    ./images/png/home/mob/icon@2x.png 2x
+  "
+  src="./images/png/home/mob/icon.png"
+  alt="cap"
+/>`,
+};

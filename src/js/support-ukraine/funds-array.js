3a8e4a36a2352e1330ebb7ccc37fcf66df511bac
DmytroBrekalo <brekalo4@gmail.com>
2023-05-08 11:41:06 +0300

    add start files

---
+++ src/js/support-ukraine/funds-array.js
@@ -0,0 +1,56 @@
+export const fundArray = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: require('../../images/png/support-ukraine/save-the-children.png'),
+    img2: require('../../images/png/support-ukraine/save-the-children@2x.png'),
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: require('../../images/png/support-ukraine/project-hope.png'),
+    img2: require('../../images/png/support-ukraine/project-hope@2x.png'),
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: require('../../images/png/support-ukraine/united24.png'),
+    img2: require('../../images/png/support-ukraine/united24@2x.png'),
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: require('../../images/png/support-ukraine/international-medical-corps.png'),
+    img2: require('../../images/png/support-ukraine/international-medical-corps@2x.png'),
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: require('../../images/png/support-ukraine/medecins-sans-frontieres.png'),
+    img2: require('../../images/png/support-ukraine/medecins-sans-frontieres@2x.png'),
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: require('../../images/png/support-ukraine/razom.png'),
+    img2: require('../../images/png/support-ukraine/razom@2x.png'),
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: require('../../images/png/support-ukraine/action-against-hunger.png'),
+    img2: require('../../images/png/support-ukraine/action-against-hunger@2x.png'),
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: require('../../images/png/support-ukraine/world-vision.png'),
+    img2: require('../../images/png/support-ukraine/world-vision@2x.png'),
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: require('../../images/png/support-ukraine/charity-foundation.png'),
+    img2: require('../../images/png/support-ukraine/charity-foundation@2x.png'),
+  },
+];
---
+++ src/js/support-ukraine/support-ukraine.js
@@ -0,0 +1,91 @@
+import { fundArray } from './funds-array';
+import Swiper from 'swiper';
+
+const supportListEl = document.querySelector('.support__list');
+const btnSwiperEl = document.querySelector('.slider-button');
+
+const renderSupportList = items => {
+  const listItems = items
+    .map((item, index) => {
+      const { title, url, img, img2 } = item;
+      const number = (index + 1).toString().padStart(2, '0');
+      return `
+           <li class="support__list-item">
+              <p class="support__number">${number}</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="company icon"
+                class="support__link"
+                href="${url}"
+              >
+                <img
+                  src="${img}"
+                  alt="${title}"
+                  class="support__logo"
+                  srcset="
+                  ${img} 1x,
+                  ${img2} 2x
+                  "
+                />
+              </a>
+              `;
+    })
+    .join('');
+  supportListEl.innerHTML = `${listItems}`;
+};
+
+renderSupportList(fundArray);
+
+const imgSlides = document.querySelectorAll('.support__list-item');
+const sliderBtn = document.querySelector('.support__btn');
+
+let counter = 0;
+let moveDown = false;
+
+const verticalSlider = function () {
+  imgSlides.forEach(function (slide) {
+    slide.style.transform = `translateY(-${counter * 100}%)`;
+  });
+};
+
+sliderBtn.addEventListener('click', function () {
+  if (moveDown) {
+    counter -= 3;
+    if (counter < 0) {
+      counter = 0;
+      moveDown = false;
+      sliderBtn.querySelector('.support__btn-icon').style.transform = '';
+    }
+  } else {
+    counter += 3;
+    if (counter >= imgSlides.length) {
+      counter = imgSlides.length - 1;
+      moveDown = true;
+      sliderBtn.querySelector('.support__btn-icon').style.transform =
+        'rotate(180deg)';
+    }
+  }
+  verticalSlider(renderSupportList);
+});
+
+// const swiper = new Swiper('.swiper', {
+//   direction: 'vertical',
+//   spaceBetween: 20,
+//   slidesPerView: 'auto',
+//   rewind: true,
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//   },
+
+//   plugins: {
+//     scrollContainer: true,
+//   },
+// });
+
+// swiper.update();
+
+// btnSwiperEl.addEventListener('click', () => {
+//   swiper.slideNext();
+// });

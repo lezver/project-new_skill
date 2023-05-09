d2858d5c052922e9c50edb810e3247f6fd0db775
Yaroslav <sych521@gmail.com>
2023-05-09 23:15:35 +0300

    First commit

--- src/js/modal-book/modal-book.js
+++ src/js/modal-book/modal-book.js
@@ -1,46 +1,75 @@
-import axios from 'axios';
+import { fetchBooks } from '../categories/fetchRequest';
 
 const listOfBooks = document.querySelector('.home__items');
 const backdrop = document.querySelector('.backdrop');
 const modalWindow = document.querySelector('.modal-window');
 const modalWindowCloseButton = document.querySelector('.close-icon');
-const bookImage = document.querySelector('.book-image');
-const bookName = document.querySelector('.book-name');
-const bookAuthor = document.querySelector('.book-author');
-const bookDescription = document.querySelector('.book-description');
-const amazonLogo = document.querySelector('.amazon-logo');
+let bookImage = document.querySelector('.book-image');
+let bookName = document.querySelector('.book-name');
+let bookAuthor = document.querySelector('.book-author');
+let bookDescription = document.querySelector('.book-description');
+const amazonLogo = document.querySelector('.amazon-logo'); //
+let amazonLink = document.querySelector('.amazon-link');
+let bookLink = document.querySelector('.book-link');
+let bookShopLink = document.querySelector('.book-shop-link');
 const addBookButton = document.querySelector('.add-book-button');
 const removeBookContainer = document.querySelector('.remove-book-container');
 const removeBookButton = document.querySelector('.remove-book-button');
 const removeBookText = document.querySelector('.remove-book-text');
-const themeSwitcher = document.querySelector('.slider__theme-switcher');
+const themeSwitcher = document.querySelector('.switch');
 const themeMode = localStorage.getItem('darkMode');
-
-console.log(themeMode);
-themeSwitcher.addEventListener('change', () => {
-  console.log('asdasdasd');
-});
-if (themeMode === 'true') {
-  modalWindow.classList.add('black-theme-modal-window');
-  modalWindowCloseButton.classList.add('black-theme-close-icon');
-  amazonLogo.classList.add('black-theme-amazon-logo');
-  addBookButton.classList.add('black-theme-add-book-button');
-  removeBookButton.classList.add('black-theme-remove-book-button');
-  removeBookText.classList.add('black-theme-remove-book-text');
-}
-
-// const test =
-//   'https://books-backend.p.goit.global/books/643282b1e85766588626a080';
-// async function fetchBook(link) {
-//   const response = await axios.get(link);
-//   return await console.log(response.data);
-// }
-// fetchBook(test);
 
 // MODAL WINDOW OPEN
 listOfBooks.addEventListener('click', e => {
   if (e.target.classList.contains('img__wrapper')) {
     backdrop.style.display = 'block';
+
+    const bookID = e.target.dataset.id;
+
+    async function fetchBook() {
+      const {
+        _id,
+        book_image,
+        title,
+        author,
+        description,
+        buy_links: [Amazon, appleBooks, , , bookShop],
+      } = await fetchBooks.getBookById(bookID);
+      bookImage.src = book_image;
+      bookName.textContent = title;
+      bookAuthor.textContent = author;
+
+      if (description) {
+        bookDescription.textContent = await description;
+      } else {
+        bookDescription.textContent = 'No description';
+      }
+
+      console.log(await fetchBooks.getBookById(bookID));
+
+      const amazonUrl = Amazon.url;
+      amazonLink.href = amazonUrl;
+
+      const bookUrl = appleBooks.url;
+      bookLink.href = bookUrl;
+
+      const bookShopUrl = bookShop.url;
+      bookShopLink.href = bookShopUrl;
+
+      // const savedBook = localStorage.getItem();
+      // if (!savedBook) {
+      //   removeBookContainer.style.display = 'none';
+      //   addBookButton.style.display = 'block';
+      // } else {
+      //   removeBookContainer.style.display = 'block';
+      //   addBookButton.style.display = 'none';
+      // }
+      // addBookButton.addEventListener('click', () => {
+      //   localStorage.setItem('shopping_list', []);
+      // });
+      return;
+    }
+    fetchBook();
   }
 });
 
@@ -52,8 +81,29 @@ document.addEventListener('click', e => {
     backdrop.style.display = 'none';
   }
 });
+
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
     backdrop.style.display = 'none';
   }
 });
+
+// console.log(themeMode);
+// themeSwitcher.addEventListener('change', () => {
+
+//   modalWindow.classList.remove('black-theme-modal-window');
+//   modalWindowCloseButton.classList.remove('black-theme-close-icon');
+//   amazonLogo.classList.remove('black-theme-amazon-logo');
+//   addBookButton.classList.remove('black-theme-add-book-button');
+//   removeBookButton.classList.remove('black-theme-remove-book-button');
+//   removeBookText.classList.remove('black-theme-remove-book-text');
+// });
+
+if (themeMode === 'true') {
+  modalWindow.classList.add('black-theme-modal-window');
+  modalWindowCloseButton.classList.add('black-theme-close-icon');
+  amazonLogo.classList.add('black-theme-amazon-logo');
+  addBookButton.classList.add('black-theme-add-book-button');
+  removeBookButton.classList.add('black-theme-remove-book-button');
+  removeBookText.classList.add('black-theme-remove-book-text');
+}

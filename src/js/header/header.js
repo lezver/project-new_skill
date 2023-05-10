cd8fcd78b716194c9fd8efab8919fdc8ff7ec866
Vladislav <102545184+lezver@users.noreply.github.com>
2023-05-10 17:06:32 +0300

    finish to the section - sign-up

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -3,7 +3,7 @@ import styles from '../../sass/utils/_variables.scss';
 import debounce from 'lodash.debounce';
 
 //   Перемикач світла/темна тема
-const body = document.body;
+export const body = document.body;
 
 const header = document.querySelector('.page-nav');
 const isDarkModeStored = localStorage.getItem('darkMode') === 'true';

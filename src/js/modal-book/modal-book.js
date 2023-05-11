98cb927796259a1261701bfbf926b49a2b134ce6
Yaroslav <sych521@gmail.com>
2023-05-11 11:06:43 +0300

    optimization and theme functional added

--- src/js/modal-book/modal-book.js
+++ src/js/modal-book/modal-book.js
@@ -17,12 +17,11 @@ const removeBookContainer = document.querySelector('.remove-book-container');
 const removeBookButton = document.querySelector('.remove-book-button');
 const removeBookText = document.querySelector('.remove-book-text');
 const themeSwitcher = document.querySelector('.switch');
-const themeMode = localStorage.getItem('darkMode');
 
 // MODAL WINDOW OPEN
 listOfBooks.addEventListener('click', e => {
   if (e.target.classList.contains('img__wrapper')) {
-    document.body.classList.add('body-owerflow-hidden');
+    document.body.classList.add('no-scroll-body-js');
 
     async function fetchBook() {
       const bookDataObj = await fetchBooks.getBookById(e.target.dataset.id);
@@ -35,28 +34,29 @@ listOfBooks.addEventListener('click', e => {
         buy_links: [Amazon, appleBooks, , , bookShop],
       } = bookDataObj;
 
+      backdrop.classList.add('backdrop-visible');
+
       modalWindow.id = await _id;
       bookImage.src = await book_image;
       bookName.textContent = await title;
       bookAuthor.textContent = await author;
-
       if (description) {
         bookDescription.textContent = description;
       } else {
         bookDescription.textContent = 'No description';
       } // ВИЯСНИТИ ЧОМУ З/БЕЗ AWAIT
-
       const amazonUrl = await Amazon.url;
       amazonLink.href = amazonUrl;
-
       const bookUrl = await appleBooks.url;
       bookLink.href = await bookUrl;
+      const bookShopUrl = await bookShop.url;
+      bookShopLink.href = await bookShopUrl;
 
-      let shoppingList = localStorage.getItem('shoppingList');
+      let shoppingList = localStorage.getItem('shopping_list');
       shoppingList = shoppingList ? JSON.parse(shoppingList) : [];
-      const index = shoppingList.find(obj => obj.id === _id);
+      const objId = shoppingList.find(obj => obj.id === _id);
 
-      if (!index) {
+      if (!objId) {
         addBookButton.classList.remove('add-book-button-none');
         removeBookContainer.classList.remove('remove-book-container-visible');
       } else {
@@ -64,24 +64,9 @@ listOfBooks.addEventListener('click', e => {
         removeBookContainer.classList.add('remove-book-container-visible');
       }
 
-      const bookShopUrl = await bookShop.url;
-      bookShopLink.href = await bookShopUrl;
-      backdrop.classList.add('backdrop-visible');
-      addBookButton.addEventListener('click', toggleBookAtShoppingList);
-      removeBookButton.addEventListener('click', makaka);
-      function makaka() {
-        const index = shoppingList.find(obj => obj.id === _id);
-        console.log(index);
-        if (index) {
-          shoppingList.splice(index, 1);
-          localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-          addBookButton.classList.remove('add-book-button-none');
-          removeBookContainer.classList.remove('remove-book-container-visible');
-          addBookButton.addEventListener('click', toggleBookAtShoppingList);
-        }
-      }
-      function toggleBookAtShoppingList() {
-        console.log('tada');
+      addBookButton.addEventListener('click', AddBookToShoppingList);
+      removeBookButton.addEventListener('click', RemoveBookFromShoppingList);
+      function AddBookToShoppingList() {
         const bookData = {
           id: _id,
           Image: book_image,
@@ -90,16 +75,26 @@ listOfBooks.addEventListener('click', e => {
           Description: description,
           buy_links: [Amazon, appleBooks, bookShop],
         };
-        if (!index) {
+        if (!objId) {
           shoppingList.push(bookData);
           addBookButton.classList.add('add-book-button-none');
           removeBookContainer.classList.add('remove-book-container-visible');
         }
-        localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-        addBookButton.removeEventListener('click', toggleBookAtShoppingList);
-        removeBookButton.removeEventListener('click', toggleBookAtShoppingList);
+        localStorage.setItem('shopping_list', JSON.stringify(shoppingList));
+        addBookButton.removeEventListener('click', AddBookToShoppingList);
+        removeBookButton.removeEventListener('click', AddBookToShoppingList);
       }
 
+      function RemoveBookFromShoppingList() {
+        const index = shoppingList.find(obj => obj.id === _id);
+        if (index) {
+          shoppingList.splice(index, 1);
+          localStorage.setItem('shopping_list', JSON.stringify(shoppingList));
+          addBookButton.classList.remove('add-book-button-none');
+          removeBookContainer.classList.remove('remove-book-container-visible');
+          addBookButton.addEventListener('click', AddBookToShoppingList);
+        }
+      }
       return;
     }
     fetchBook();
@@ -109,37 +104,39 @@ listOfBooks.addEventListener('click', e => {
 // MODAL WINDOW CLOSE
 document.addEventListener('click', e => {
   if (e.target === backdrop) {
-    document.body.style.overflow = 'auto';
     backdrop.classList.remove('backdrop-visible');
+    document.body.classList.remove('no-scroll-body-js');
   } else if (e.target === modalWindowCloseButton) {
-    document.body.style.overflow = 'auto';
     backdrop.classList.remove('backdrop-visible');
+    document.body.classList.remove('no-scroll-body-js');
   }
 });
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
-    document.body.style.overflow = 'auto';
     backdrop.classList.remove('backdrop-visible');
+    document.body.classList.remove('no-scroll-body-js');
   }
 });
 
-// console.log(themeMode);
-// themeSwitcher.addEventListener('change', () => {
-
-//   modalWindow.classList.remove('black-theme-modal-window');
-//   modalWindowCloseButton.classList.remove('black-theme-close-icon');
-//   amazonLogo.classList.remove('black-theme-amazon-logo');
-//   addBookButton.classList.remove('black-theme-add-book-button');
-//   removeBookButton.classList.remove('black-theme-remove-book-button');
-//   removeBookText.classList.remove('black-theme-remove-book-text');
-// });
-
-if (themeMode === 'true') {
-  modalWindow.classList.add('black-theme-modal-window');
-  modalWindowCloseButton.classList.add('black-theme-close-icon');
-  amazonLogo.classList.add('black-theme-amazon-logo');
-  addBookButton.classList.add('black-theme-add-book-button');
-  removeBookButton.classList.add('black-theme-remove-book-button');
-  removeBookText.classList.add('black-theme-remove-book-text');
+// THEME MOD TOGGLE
+themeSwitcher.addEventListener('change', changeThemeFunc);
+changeThemeFunc();
+function changeThemeFunc() {
+  const themeMode = localStorage.getItem('darkMode');
+  if (themeMode === 'true') {
+    modalWindow.classList.add('black-theme-modal-window');
+    modalWindowCloseButton.classList.add('black-theme-close-icon');
+    amazonLogo.classList.add('black-theme-amazon-logo');
+    addBookButton.classList.add('black-theme-add-book-button');
+    removeBookButton.classList.add('black-theme-remove-book-button');
+    removeBookText.classList.add('black-theme-remove-book-text');
+  } else {
+    modalWindow.classList.remove('black-theme-modal-window');
+    modalWindowCloseButton.classList.remove('black-theme-close-icon');
+    amazonLogo.classList.remove('black-theme-amazon-logo');
+    addBookButton.classList.remove('black-theme-add-book-button');
+    removeBookButton.classList.remove('black-theme-remove-book-button');
+    removeBookText.classList.remove('black-theme-remove-book-text');
+  }
 }
